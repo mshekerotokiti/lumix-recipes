e01967ef866e83574728aa29b33826399c59f117
+ <+>
2023-06-08 11:52:46 +0300

    Create cartItem class

---
+++ app/models/cart_item.rb
@@ -0,0 +1,19 @@
+class CartItem < ActiveRecord::Base
+    belongs_to :ingredient
+    belongs_to :recipe
+
+    def self.display_ingredients
+        self.all.map do |c|
+        c.ingredient
+        end
+end
+
+
+    def self.display_recipes
+        self.all.map do |c|
+        c.recipe
+        end
+    end
+
+
+end
