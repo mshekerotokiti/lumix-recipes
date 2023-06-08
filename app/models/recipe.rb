239e251385bb2a6a7d735799ec53b0def5811ae4
+ <+>
2023-06-08 11:55:03 +0300

    Create recipe.rb file

---
+++ app/models/recipe.rb
@@ -0,0 +1,18 @@
+class Recipe < ActiveRecord::Base
+    has_many :ingredients
+    has_many :cart_items
+    # has_many :ingredients, through: :cart_items
+
+
+
+
+    def self.post_ingredients(id)
+        self.find(id).ingredients.map do |t|
+            CartItem.create(
+                recipe_id: id,
+                ingredient_id: t.id
+            )
+        end 
+    end
+
+end
